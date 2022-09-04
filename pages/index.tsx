9906e38d0c83c96a3dbb33f101e506cb7c3557f1
Jose B <jose.zdy@gmail.com>
2022-09-03 21:40:40 -0500

    validate mint ownership, add SVG icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,41 @@
+import { Metadata, Metaplex } from '@metaplex-foundation/js';
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+// import bs58 from 'bs58';
+// import { toUint8Array } from 'js-base64';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 import { useEffect, useState } from 'react';
 import { QrReader } from "react-qr-reader";
+// import nacl from 'tweetnacl';
 
-const validateMintOwnership = async (signature: string, owner: string, mint: string) => {
-  console.log({ signature, owner, mint })
-  return false
+const connection = new Connection(clusterApiUrl("devnet"));
+const mx = Metaplex.make(connection);
+
+const validateMintOwnership = async (signature: string, ownerHash: string, mintHash: string) => {
+  console.log({ signature, ownerHash, mintHash })
+  const owner = new PublicKey(ownerHash)
+  const mint = new PublicKey(mintHash)
+
+  // Validate signature
+  // const sig = toUint8Array(signature)
+  // console.log("sig", sig)
+  // const verified = nacl
+  //   .sign
+  //   .detached
+  //   .verify(
+  //     new TextEncoder().encode(`${ownerHash}:${mintHash}`),
+  //     sig,
+  //     owner.toBytes()
+  //   )
+  // if (!verified) return false
+
+  // Validate mint ownership
+  const metadatas = await mx.nfts().findAllByOwner({ owner }).run() as Metadata[]
+  const ownsMint = !!metadatas.find(md => mint.equals(md.mintAddress))
+  if (!ownsMint) return false
+
+  return true
 }
 
 enum ValidationStatus {
@@ -28,7 +57,7 @@ const Home: NextPage = () => {
         beep.play()
         setShowModal(true)
         setValidationStatus(ValidationStatus.LOADING)
-  
+
         const [signature, owner, mint] = qrString.split(':')
         const valid = await validateMintOwnership(signature, owner, mint)
         setValidationStatus(valid ? ValidationStatus.APPROVED : ValidationStatus.REJECTED)
@@ -86,10 +115,20 @@ const Home: NextPage = () => {
 
                   {/*body*/}
                   <div className="relative p-6 flex-auto">
-                    <svg aria-hidden="true" className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                    </svg>
+                    {validationStatus == ValidationStatus.LOADING && (
+                      <svg aria-hidden="true" className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                      </svg>
+                    )}
+
+                    {validationStatus == ValidationStatus.REJECTED && (
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" /></svg>
+                    )}
+
+                    {validationStatus == ValidationStatus.APPROVED && (
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
+                    )}
                   </div>
 
                   {/*footer*/}
